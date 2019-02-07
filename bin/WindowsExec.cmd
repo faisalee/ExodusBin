f5d193a43bbfa28a61272daca8422e2d0b267361
Faisal Saeed <faisal@alteredstar.com>
2019-02-08 02:51:39 +0800

    Implemented SQL_MODE for both Source and Target
    This automates the SQL_MODE for both Source and Target Connections

--- bin/WindowsExec.cmd
+++ bin/WindowsExec.cmd
@@ -1,6 +1,6 @@
 @echo off
 cls
-set CLASSPATH=.;C:\Work\Java\Exodus\bin\resources\mariadb-java-client-2.3.0.jar;C:\Work\Java\Exodus\bin\resources\mysql-connector-java-8.0.12.jar
+set CLASSPATH=.;C:\Users\faisa\OneDrive\Work\Java\Exodus\bin\resources\mariadb-java-client-2.3.0.jar;C:\Users\faisa\OneDrive\Work\Java\Exodus\bin\resources\mysql-connector-java-8.0.12.jar
 
 mkdir resources>nul 2>nul
 mkdir ddl>nul 2>nul
@@ -12,4 +12,4 @@ copy *.xml resources>nul 2>nul
 copy *.properties resources>nul 2>nul
 
 @echo on
-java -Xms6196m -Xmx12240m mariadb.migration.Exodus
+java -Xms6196m -Xmx10240m mariadb.migration.Exodus
